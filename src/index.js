3543684ad23aee723d60b28abeea890ebf624582
dionkeldei <dionkeldei@gmail.com>
2021-07-22 11:19:27 -0500

    js ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hola')
+import UrlParams from './urlParams'
+import Cookies from './cookie'
+
+var params = UrlParams.get()
+
+function init(){
+  for(var x in params){
+    Cookie.set(x,params[x],1)
+  }
+}
+
+init()
---
+++ src/urlParams.js
@@ -0,0 +1,12 @@
+const obj = {
+  get: function () {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+  }
+}
+
+module.exports = obj
